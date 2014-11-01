4ce252a7162e7a065f4197e32507c2e05d4cd410
Rafael Figueiredo <raffccc@gmail.com>
2014-10-31 23:57:59 -0200

    teste

--- soa-cap05-rest/src/main/resources/import.sql
+++
@@ -1,5 +0,0 @@
-insert into User(name,login,password) values ('Alexandre','admin','admin');
-insert into User(name,login,password) values ('Rafael','rafael','rafael');
-insert into User(name,login,password) values ('Paulo','paulo','paulo');
-insert into User(name,login,password) values ('Adriano','adriano','adriano');
-insert into User(name,login,password) values ('Matheus','matheus','matheus');
